d1abab73e81dc4d381ef6ca4c357abedc482a133
vikash-kum <vikashkumar09852@gmail.com>
2024-08-08 20:57:53 +0530

    change in the port number

--- main.go
+++ main.go
@@ -10,8 +10,8 @@ func main() {
 	r := routes.SetupRouter()
 
 	// Start the server
-	log.Println("Starting server on :8080")
-	if err := r.Run(":8080"); err != nil {
+	log.Println("Starting server on :8000")
+	if err := r.Run(":8000"); err != nil {
 		log.Fatalf("Could not start server: %v\n", err)
 	}
 }
